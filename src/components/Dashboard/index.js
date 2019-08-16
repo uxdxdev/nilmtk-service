e2bf9bea188c389d0fe6b053b78a2d3076197d25
DMorton <dmorton@ie.ibm.com>
2019-08-17 00:30:30 +0100

    Added push notifications

--- src/components/Dashboard/index.js
+++ src/components/Dashboard/index.js
@@ -1,10 +1,36 @@
 import React from 'react';
+import { messaging } from '../../init-fcm';
 
 class Dashboard extends React.Component {
   constructor(props) {
     super(props);
     this.textInput = React.createRef();
     this.inputErrorMessage = React.createRef();
+
+    this.state = {
+      messages: []
+    };
+  }
+
+  async componentDidMount() {
+    messaging
+      .requestPermission()
+      .then(async function() {
+        const token = await messaging.getToken();
+        console.log(token);
+      })
+      .catch(function(err) {
+        console.log('Unable to get permission to notify.', err);
+      });
+    navigator.serviceWorker.addEventListener('message', message => {
+      console.log(message);
+      const { data } = message;
+      const messageObject = data['firebase-messaging-msg-data'];
+      const { notification } = messageObject;
+      const { messages } = this.state;
+      messages.push(notification);
+      this.setState({ messages });
+    });
   }
 
   sendMessage = () => {
@@ -24,18 +50,26 @@ class Dashboard extends React.Component {
     }
   };
   render() {
+    const { messages } = this.state;
     return (
       <>
         <p>This is the dashboard page.</p>
 
-        <input
+        {/* <input
           type="text"
           ref={this.textInput}
           required
           pattern="[A-Za-z]{1,20}"
         />
         <p ref={this.inputErrorMessage} />
-        <button onClick={this.sendMessage}>Send</button>
+        <button onClick={this.sendMessage}>Send</button> */}
+        <ul>
+          {messages.map((message, index) => (
+            <li key={index}>
+              {message.title} {message.body}
+            </li>
+          ))}
+        </ul>
       </>
     );
   }
--- src/index.js
+++ src/index.js
@@ -2,7 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-// import * as serviceWorker from './serviceWorker';
+import * as serviceWorker from './serviceWorker';
+
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker
+    .register('./firebase-messaging-sw.js')
+    .then(function(registration) {
+      console.log('Registration successful, scope is:', registration.scope);
+    })
+    .catch(function(err) {
+      console.log('Service worker registration failed, error:', err);
+    });
+}
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
@@ -10,3 +21,4 @@ ReactDOM.render(<App />, document.getElementById('root'));
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 // serviceWorker.unregister();
+serviceWorker.register();
