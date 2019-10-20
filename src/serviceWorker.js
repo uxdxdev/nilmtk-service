58f6fb508fb44914b3d94c9e6e9e66c0082de2cf
DMorton <dmorton@ie.ibm.com>
2019-10-20 10:51:41 +0100

    fix(serviceWorker.js): 🐛 Bug fixes:
    -

--- src/serviceWorker.js
+++ src/serviceWorker.js
@@ -74,6 +74,7 @@ function registerValidSW(swUrl, config) {
                   'tabs for this page are closed. See https://bit.ly/CRA-PWA.'
               );
 
+              console.log('reloading to update');
               window.location.reload();
 
               // Execute callback
